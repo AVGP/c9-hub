d1518cc4094ff9c047cc485c0c974a69a491912a
israelidanny <dannypovolotski@gmail.com>
2014-03-24 08:28:58 -0400

    Added port detection. We're not just blindly assigning ports now

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.locals.cache = 'memory';
 // Prettify HTML
 app.locals.pretty = true;
 
-var nextFreeWorkspacePort = 5000;
+app.set('nextFreeWorkspacePort', 5000);
 
 app.engine('html', consolidate.swig);
 
@@ -87,15 +87,6 @@ app.use(passport.session());
 app.use(flash());
 // Dynamic helpers
 app.use(helpers('Cloud9Hub'));
-app.use(function(req, res, next) {
-    if(/^\/workspace\/[^\/]+\/?$/.test(req.path)) {
-        req.nextFreePort = (nextFreeWorkspacePort++);
-        if(nextFreeWorkspacePort > 10000) {
-          nextFreeWorkspacePort = 5000;
-        }
-    }
-    next();
-});
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
